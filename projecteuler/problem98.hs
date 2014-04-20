f89c90a245ec9113c3d684aa810c6630549e0fac
Mikhail Puzanov <mpuzanov@ripe.net>
2014-04-21 01:31:27 +0200

    Solved the problem

--- projecteuler/problem98.hs
+++ projecteuler/problem98.hs
@@ -3,7 +3,8 @@ import Data.Char (digitToInt)
 import System.IO
 import qualified Data.List as List
 import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.Maybe
+import qualified Data.Map as M
 
 import Util
 
@@ -17,50 +18,54 @@ readLines = do
         hClose handle
         return list
 
-equalSets :: Eq a => [a] -> [a] -> Bool
-equalSets x y = nested x y && nested y x
-    where nested :: Eq b => [b] -> [b] -> Bool
-          nested [] _ = True
-          nested ps [] = False
-          nested (p:ps) qs = any ((==) p) qs && nested ps qs
-
 anagramClasses :: [String] -> [[String]]
 anagramClasses [] = []
 anagramClasses (w:words) = (w:anagrams) : anagramClasses notAnagrams 
     where (anagrams, notAnagrams) = List.partition (anagram w) words
-          anagram word1 word2     = length word1 == length word2 && equalSets word1 word2 
-
-unique :: Eq a => [a] -> [a]
-unique [] = []
-unique z@(x:xs) = if (any ((==) x) xs) then xs else z 
-
+          anagram word1 word2     = List.sort word1 == List.sort word2
 
 type EncMap = Map Char Int 
 
-encodings :: String -> [[Int]]
-encodings [] = []
-encodings s = encMap s (Map.fromList [] :: EncMap)
+encodingMappings :: String -> [EncMap]
+encodingMappings word = encMap word	(M.empty :: EncMap)
   where
-    encMap :: String -> EncMap -> [[Int]]
-    encMap [] _ = [[]]
-    encMap (c : cs) mapping = case Map.lookup c mapping of 
-      Just digit -> map (digit : ) (encMap cs mapping)
-      Nothing -> flatten $ map makeOptions $ filter (\d -> not (d `elem` (Map.elems mapping))) [0..9]
+    encMap :: String -> EncMap -> [EncMap]
+    encMap [] mapping = [mapping]
+    encMap (c : cs) mapping = case M.lookup c mapping of 
+      Just digit -> encMap cs mapping
+      Nothing -> concatMap subMappings feasibleDigits
         where
-          makeOptions digit = [ digit : rest | rest <- encMap cs (Map.insert c digit mapping)] 
-          flatten list = foldr (++) [] list
+          subMappings digit = encMap cs (M.insert c digit mapping)
+          feasibleDigits = filter (\d -> notElem d (M.elems mapping)) [0..9]
 
-squareEncodings s = filter isSquare $ map number (encodings s)
+encode :: EncMap -> String -> Maybe Int
+encode mapping word = case map (mappingToEnc mapping) word of
+      0 : _ -> Nothing
+      d @ _ -> Just (read (concatMap show d) :: Int)
   where
-    number :: [Int] -> Int
-    number digits = read (foldr (++) "" (map show digits)) :: Int
-    isSquare d = case intSqrt d of
-      Nothing -> False
-      Just _ -> True
+    mappingToEnc mapping char = case M.lookup char mapping of
+      Just digit -> digit
 
-maxSquare :: [String] -> Int
-maxSquare words = maximum $ foldr (++) [] $ map (\ac -> map squareEncodings ac) (anagramClasses words) 
+squareEncodings :: String -> [(EncMap, Int)]
+squareEncodings s = [ (encMap, num) | (encMap, num) <- encodings s, (isJust . intSqrt) num ]
+  where
+    encodings s = [ (m, code) | (m, Just code) <- map (\m -> (m, encode m s)) $ encodingMappings s ]
+
+
+squareAnagramClass :: [String] -> Int
+squareAnagramClass [] = 0
+squareAnagramClass (word : ws) = if null otherSquares then 0 else maximum otherSquares
+  where 
+	otherSquares = [ max num otherNum | (encMap, num) <- squareEncodings word, Just otherNum <- map (encode encMap) ws, (isJust . intSqrt) otherNum ]
+
 
 main = do
   lines <- readLines  
-  print $ maxSquare $ makeList lines
+  --print $ map (map encodings) $ filter ((>1) . length) $ anagramClasses $ makeList lines
+  --print $ filter ((>1) . length) $ anagramClasses $ makeList lines
+  print $ squareEncodings "ABB"
+  let classes = filter ((>1) . length) $ anagramClasses $ makeList lines
+  print $ classes
+  --print $ squareEncodings $ head $ head $ classes	
+  print $ maximum $ map squareAnagramClass $ classes
+
