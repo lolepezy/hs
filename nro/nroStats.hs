f9c8f514814cde1ff1fe455be9a691cf56fb43e5
Mikhail Puzanov <mpuzanov@ripe.net>
2016-03-15 23:19:31 +0100

    Add QuckCheck tests, fix

--- nro/nroStats.hs
+++ nro/nroStats.hs
@@ -1,4 +1,5 @@
-import Data.List (sortBy)
+import           Data.List       (sortBy)
+import           Test.QuickCheck
 
 data Interval i p = Interval i i (Payload p)
   deriving (Show, Eq)
@@ -14,82 +15,68 @@ intersect (Interval b1 e1 _) (Interval b2 e2 _) =
   b1 < e2 && b1 >= b2 || b2 < e1 && b2 >= b1
 
 
-compareI :: Ord i => Interval i p -> Interval i p -> Ordering
-compareI (Interval b1 e1 pay1) (Interval b2 e2 pay2) =
-  compareAnd (compare b1 b2) $ compareAnd (compare e1 e2) $ compare (priority pay1) (priority pay2)
-  where
-    compareAnd c f = case c of
-      LT -> LT
-      GT -> GT
-      EQ -> f
-
-
-merge :: Ord i => [Interval i p] -> [Interval i p]
+merge :: (Ord i, Show i, Show p) => [Interval i p] -> [Interval i p]
 merge [] = []
-merge ints = concatMap resolve $
+merge ints = filter (\(Interval b e _) -> b /= e) $
+  concatMap resolve $
   divideToSegments $ sortBy compareI ints
-
-
-resolve [] = []
-resolve (s:others) = go [s] others
   where
-    go divided [] = divided
-    go divided (s1 : ss) = go (s1 `addTo` divided) ss
+    compareI (Interval b1 _ _) (Interval b2 _ _) = compare b1 b2
+
+    resolve [] = []
+    resolve (s:others) = foldl (flip addTo) [s] others
+
+    addTo (Interval bs es ps) dest = filter (\(Interval b e _) -> b /= e) $ go dest
+      where
+        srcApplied dst@(Interval bd ed pd)
+          | bs >= ed                         = [ dst ]
+          | bs >= bd && bs <= ed && es > ed  = [ Interval bd bs pd, Interval bs ed (winner ps pd) ]
+          | bs >= bd && es <= ed             = [ Interval bd bs pd, Interval bs es (winner ps pd), Interval es ed pd ]
+          | bs < bd  && es > ed              = [ Interval bd ed (winner ps pd) ]
+          | bs < bd  && es <= ed && es > bd  = [ Interval bd es (winner ps pd) ]
+          | es <= bd                          = [ dst ]
+
+        go [] = []
+        go [d@(Interval _ ed _)] = srcApplied d ++ [ Interval ed es ps | es > ed ]
+        go (d : dests)           = srcApplied d ++ go dests
+
+        winner p1 p2 = if priority p1 < priority p2 then p1 else p2
+
+    divideToSegments [] = []
+    divideToSegments (i:iis) =
+      case splitIntersecting [i] iis of
+        ([], x)             -> [x]
+        (x, [])             -> [x]
+        (segment, leftover) -> segment : divideToSegments leftover
+      where
+        splitIntersecting b [] = (b, [])
+        splitIntersecting b x@(ii:is)
+          | any (`intersect` ii) b = splitIntersecting (b ++ [ii]) is
+          | otherwise             = (b, x)
 
 
-addTo :: Ord i => Interval i p -> [Interval i p] -> [Interval i p]
-addTo src@(Interval bs es ps) dest = filter (\(Interval b e _) -> b /= e) $ go dest
-  where
-    -- bs = 7
-    -- es = 20
-    -- bd = 15
-    -- ed = 18
 
-    srcApplied dst@(Interval bd ed pd)
-      | bs >= ed                         = [ dst ]
-      | bs >= bd && bs <= ed && es > ed  = [ Interval bd bs pd, Interval bs ed (winner ps pd) ]
-      | bs >= bd && es <= ed             = [ Interval bd bs pd, Interval bs es (winner ps pd), Interval es ed pd ]
-      | bs < bd  && es > ed              = [ Interval bd ed (winner ps pd) ]
-      | bs < bd  && es <= ed && es > bd  = [ Interval bd es (winner ps pd) ]
-      | es < bd                          = [ dst ]
+prop_noEmptyIntervals :: [Interval Int Int] -> Bool
+prop_noEmptyIntervals ints =
+  let merged = merge ints
+    in not $ any (\(Interval b e _) -> b == e) merged
 
-    go [] = []
-    go [d@(Interval _ ed _)] = srcApplied d ++ [ Interval ed es ps | es > ed ]
-    go (d : dests)           = srcApplied d ++ go dests
+prop_nonIntersecting :: [Interval Int Int] -> Bool
+prop_nonIntersecting ints =
+  let merged = merge ints
+    in null [ (i1, i2) | i1 <- merged, i2 <- merged, i1 /= i2 && i1 `intersect` i2]
 
-    winner p1 p2 = if priority p1 < priority p2 then p1 else p2
 
-divideToSegments [] = []
-divideToSegments (i:ints) =
-  case splitIntersecting [i] ints of
-    ([], x)             -> [x]
-    (x, [])             -> [x]
-    (segment, leftover) -> segment : divideToSegments leftover
-  where
-    splitIntersecting b [] = (b, [])
-    splitIntersecting b x@(ii:is)
-      | any (`intersect` ii) b = splitIntersecting (b ++ [ii]) is
-      | otherwise             = (b, x)
-
+instance (Ord i, Arbitrary i, Arbitrary p) => Arbitrary (Interval i p) where
+  arbitrary = do
+    b <- arbitrary
+    e <- suchThat arbitrary (>b)
+    p <- arbitrary
+    x <- arbitrary
+    return $ Interval b e (Payload p x)
 
 
 main :: IO ()
 main = do
-  print $ compareI (Interval 1 3 (Payload 0 ())) (Interval 2 5 (Payload 0 ())) == LT
-  print $ compareI (Interval 1 3 (Payload 0 ())) (Interval 1 4 (Payload 0 ())) == LT
-  print $ compareI (Interval 1 3 (Payload 0 ())) (Interval 1 3 (Payload 1 ())) == LT
-  print $ compareI (Interval 1 3 (Payload 0 ())) (Interval 1 2 (Payload 1 ())) == GT
-  print $ compareI (Interval 1 3 (Payload 0 ())) (Interval 1 3 (Payload 0 ())) == EQ
-  print $ not $ intersect (Interval 1 2 (Payload 0 ())) (Interval 2 5 (Payload 0 ()))
-  print $ intersect (Interval 2 3 (Payload 0 ())) (Interval 2 5 (Payload 0 ()))
-  print $ not $ intersect (Interval 2 5 (Payload 0 ())) (Interval 1 2 (Payload 0 ()))
-
-  let i1 = Interval 0 18 (Payload 2 ())
-  let i2 = Interval 5 15 (Payload 0 ())
-  let i3 = Interval 7 20  (Payload 1 ())
-  let i4 = Interval 25 30 (Payload 0 ())
-  let i5 = Interval 27 32 (Payload 1 ())
-
-  let list = [ i1, i2, i3, i4 ]
-  print list
-  print $ merge list
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_noEmptyIntervals
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_nonIntersecting
